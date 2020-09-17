63ee225e04d131658a9c8701ba19672903bd9f69
paskuale75 <pellicaipasquale@gmail.com>
2020-09-17 23:09:25 +0200

    main widget updated

--- src/Uppyuploader.php
+++ src/Uppyuploader.php
@@ -2,9 +2,45 @@
 
 namespace paskuale75\uppy;
 
+use paskuale75\uppy\assets\UppyAsset;
 use yii\base\Widget;
 
 
-class Uppyuploader extends Widget{
+class Uppyuploader extends Widget
+{
 
+    public $options = []; //
+    public $clientOptions = [];
+
+    public function init()
+    {
+        parent::init();
+    }
+
+    public function run()
+    {
+        $id = $this->getId();
+        $this->registerPlugin();
+        $this->registerJS();
+    }
+
+
+    protected function registerPlugin()
+    {
+        $view = $this->getView();
+        UppyAsset::register($view);
+    }
+
+    protected function registerJs()
+    {
+        $options = json_encode($this->options);
+        $clientOptions = json_encode($this->clientOptions);
+
+        $js = <<<JS
+var uppy = Uppy.Core()
+  uppy.use(Uppy.DragDrop, { target: '#drag-drop-area' })
+  uppy.use(Uppy.Tus, { endpoint: 'https://master.tus.io/files/' })
+JS;
+        $this->view->registerJs($js);
+    }
 }
--- src/assets/UppyAsset.php
+++ src/assets/UppyAsset.php
@@ -1,9 +1,20 @@
 <?php
+namespace paskuale75\uppy\assets;
 
 
 use yii\web\AssetBundle;
 
 
 class UppyAsset extends AssetBundle{
-    
+    public $sourcePath = '@bower/uppy/';
+    public $js = [
+        'src/core/Core.js'
+    ];
+    public $css = [
+        '/core/dist/style.css',
+        '/dashboard/dist/style.css'
+    ];
+    public $depends = [
+        'yii\web\JqueryAsset',
+    ];
 }
