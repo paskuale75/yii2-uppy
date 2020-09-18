6c8291fc001385c893c0dcb9a23e763569b32ce5
pasquale75 <pellicanipasquale@gmail.com>
2020-09-18 11:49:08 +0200

    js map removed

--- src/assets/UppyAsset.php
+++ src/assets/UppyAsset.php
@@ -7,7 +7,6 @@ class UppyAsset extends AssetBundle{
     public $sourcePath = '@npm/uppy/dist';
     public $js = [
         'uppy.min.js',
-        'uppy.min.js.map'
     ];
     public $css = [
         'uppy.min.css',
