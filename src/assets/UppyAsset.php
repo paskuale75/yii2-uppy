80965a1a7f6e37f397dc263917aa2a063bb07484
pasquale75 <pellicanipasquale@gmail.com>
2020-09-18 11:00:59 +0200

    js minify

--- src/assets/UppyAsset.php
+++ src/assets/UppyAsset.php
@@ -6,8 +6,8 @@ use yii\web\AssetBundle;
 class UppyAsset extends AssetBundle{
     public $sourcePath = '@npm/uppy/dist';
     public $js = [
-        'uppy.js',
-        'uppy.js.map'
+        'uppy.min.js',
+        'uppy.min.js.map'
     ];
     public $css = [
         'uppy.min.css',
