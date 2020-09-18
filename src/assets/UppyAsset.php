1a8c1cc9a67a95a93c5dc39718552d400003767c
pasquale75 <pellicanipasquale@gmail.com>
2020-09-18 10:49:31 +0200

    uppy.js.map added to js array

--- src/assets/UppyAsset.php
+++ src/assets/UppyAsset.php
@@ -6,7 +6,8 @@ use yii\web\AssetBundle;
 class UppyAsset extends AssetBundle{
     public $sourcePath = '@npm/uppy/dist';
     public $js = [
-        'uppy.js'
+        'uppy.js',
+        'uppy.js.map'
     ];
     public $css = [
         'uppy.min.css',
