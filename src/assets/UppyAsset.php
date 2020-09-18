1dff314ddc806597ea962f3e3ee99859305ef842
pasquale75 <pellicanipasquale@gmail.com>
2020-09-18 08:59:44 +0200

    npm paths fix

--- src/assets/UppyAsset.php
+++ src/assets/UppyAsset.php
@@ -4,13 +4,12 @@ namespace paskuale75\uppy\assets;
 use yii\web\AssetBundle;
 
 class UppyAsset extends AssetBundle{
-    public $sourcePath = '@bower/uppy';
+    public $sourcePath = '@npm/uppy';
     public $js = [
-        'bin/build-lib.js'
+        'uppy.js'
     ];
     public $css = [
-        '/core/dist/style.css',
-        '/dashboard/dist/style.css'
+        'uppy.min.css',
     ];
     public $depends = [
         'yii\web\JqueryAsset',
