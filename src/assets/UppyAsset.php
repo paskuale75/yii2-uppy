b19a270e6f6ebe39ae57619e8e62f6faa9fc6615
paskuale75 <pellicaipasquale@gmail.com>
2020-09-20 22:58:33 +0200

    asset updated

--- src/assets/UppyAsset.php
+++ src/assets/UppyAsset.php
@@ -5,22 +5,19 @@ use Yii;
 use yii\web\AssetBundle;
 
 class UppyAsset extends AssetBundle{
-    public $sourcePath = '@npm/uppy/dist';
+
+    public $sourcePath = '@npm/';
     
     public function init(){
-        $lang = str_replace('-','_',Yii::$app->language);
+        //$lang = str_replace('-','_',Yii::$app->language);
         $this->js = [
-            'uppy.min.js',
-            "https://transloadit.edgly.net/releases/uppy/locales/v1.16.7/{$lang}.min.js"
+            "uppy--locales/dist/it_IT.min.js", //<- don't work
+            "uppy/dist/uppy.min.js",
+            //"https://transloadit.edgly.net/releases/uppy/locales/v1.16.7/{$lang}.min.js"
         ];
     }
 
     public $css = [
-        'uppy.min.css',
+        'uppy/dist/uppy.min.css',
     ];
-    /*
-    public $depends = [
-        'yii\web\JqueryAsset',
-    ];
-    */
 }
